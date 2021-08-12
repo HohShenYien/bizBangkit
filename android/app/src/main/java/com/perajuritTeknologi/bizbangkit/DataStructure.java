e6f980eba1acd4dc9d7d510fca2d0b8b21edae4b
Chong Rui Jun <chongruijun@gmail.com>
2021-08-12 16:44:54 +0800

    added gender spinner in registration page, added DataConversion.java

--- android/app/src/main/java/com/perajuritTeknologi/bizbangkit/DataStructure.java
+++ android/app/src/main/java/com/perajuritTeknologi/bizbangkit/DataStructure.java
@@ -11,14 +11,18 @@ public class DataStructure {
         public String name;
         public String nric;
         public String phoneNumber;
+        public String gender;
+        public String profilePicture;
         public String username;
         public String email;
         public String password;
 
-        UserProfileDetails(String name, String nric, String phoneNumber, String username, String email, String password) {
+        UserProfileDetails(String name, String nric, String phoneNumber, String gender, String profilePicture, String username, String email, String password) {
             this.name = name;
             this.nric = nric;
             this.phoneNumber = phoneNumber;
+            this.gender = gender;
+            this.profilePicture = profilePicture;
             this.username = username;
             this.email = email;
             this.password = password;
